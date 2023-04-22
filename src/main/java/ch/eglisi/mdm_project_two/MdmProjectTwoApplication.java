efc8acafeb3230944cc8323508425f7b5baaf0a9
Simon Egli <eglisi1@students.zhaw.ch>
2023-04-22 07:48:01 +0200

    create project with Spring Boot initializr

---
+++ src/main/java/ch/eglisi/mdm_project_two/MdmProjectTwoApplication.java
@@ -0,0 +1,13 @@
+package ch.eglisi.mdm_project_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmProjectTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdmProjectTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/eglisi/mdm_project_two/MdmProjectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.eglisi.mdm_project_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmProjectTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
