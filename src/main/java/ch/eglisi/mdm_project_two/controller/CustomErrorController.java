3ed1876c056a07864dbf7109bd088a5b0d075d1a
Simon Egli <eglisi1@students.zhaw.ch>
2023-04-22 08:04:34 +0200

    add custom error handler controller

--- src/main/java/ch/eglisi/mdm_project_two/controller/CustomErrorController.java
+++ src/main/java/ch/eglisi/mdm_project_two/controller/CustomErrorController.java
@@ -1,2 +1,37 @@
-package ch.eglisi.mdm_project_two.controller;public class CustomErrorController {
+package ch.eglisi.mdm_project_two.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request) {
+        Object status = request.getAttribute("javax.servlet.error.status_code");
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            httpStatus = HttpStatus.valueOf(statusCode);
+        }
+
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("status", httpStatus.value());
+        modelAndView.addObject("error", httpStatus.getReasonPhrase());
+        return modelAndView;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception ex) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        modelAndView.addObject("error", ex.getMessage());
+        return modelAndView;
+    }
 }
